f611c7da9e81b147d8cb9ebb95fc3301c1163c7b
Henrique <henrique.schinaider@live.com>
2025-06-28 06:52:59 -0300

    Anotacoes

---
+++ madlib.py
@@ -0,0 +1,16 @@
+nome = input('Digite um nome: ')
+verbo_infinitivo = input('Digite um verbo no infinitivo: ')
+linguagem_programacao = input('Digite uma linguagem de programacao: ')
+funcao_famosa = input('Digite uma funcao do python: ')
+numero = input('Digite um numero: ')
+comida = input('Digite uma comida: ')
+bug = input('Digite um bug comum: ')
+adjetivo = input('Digite um adjetivo: ')
+objeto = input ('Digite um objetivo: ')
+sentimento = input('Digite um sentimento: ')
+
+print('Era uma vez um(a) programador(a) chamado(a) ' + nome + ',que adorava ' + verbo_infinitivo + ' em ' + linguagem_programacao + '.' +
+'Um dia, decidiu criar uma função chamada ' +  funcao_famosa + ' , que faria exatamente ' + numero + ' coisas diferentes com uma ' + comida + '.'
+'Mas algo deu errado! Ao rodar o código, surgiu um ' + bug + ' terrível que deixou tudo completamente ' + adjetivo + '.'
+' Sem perder tempo, '+ nome + ' pegou seu(a) ' + objeto + ' e começou a debugar com muita ' +  sentimento + ' no coração.'
+'No final, o programa funcionou perfeitamente… e fez a ' + comida + ' dançar na tela. Foi um sucesso total!')
---
+++ magica.py
@@ -0,0 +1,7 @@
+import os
+print('Escolha uma carta')
+print('J♣️ Q♥️ 7♦️ A♣️')
+input()
+os.system('clear')
+print('Sumiu...')
+print('A♣️ J♥️ A♦️ 7♣️')
