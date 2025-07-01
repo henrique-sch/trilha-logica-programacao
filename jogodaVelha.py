8a71888dc5f3420fb189e3f72cf85acb3d2685c1
Henrique <henrique.schinaider@live.com>
2025-07-01 07:43:18 -0300

    Jogo da Velha Finalizado

--- jogodaVelha.py
+++ jogodaVelha.py
@@ -1,11 +1,14 @@
-# sao tres linhas e tres colunas com espaços em brancos
-# vai ser apontado a posicao e onde o jogador digitar é substituido pelo valor
-# tres listas com os espaços dos numeros
+jogador = 'X'
+def exibeTabuleiro():
+    for linha in tabuleiro:
+        print(f'{linha[0]} | {linha[1]} | {linha[2]}')
 
-# tabuleiro = [[' ' for i in range(3)] for i in range(3)]
-
-print(tabuleiro)
-faopkf
+def jogada(linha, coluna):
+    if tabuleiro[linha][coluna] == 'X' or tabuleiro[linha][coluna] == 'O':
+        print('Posição inválida')
+        return jogador
+    tabuleiro[linha][coluna] = jogador
+    return 'O' if jogador == 'X' else 'X'
 
 tabuleiro = [
     ['00', '01', '02'],
@@ -13,32 +16,67 @@ tabuleiro = [
     ['20', '21', '22']
 ]
 
-print(tabuleiro[0])
-print(tabuleiro[1])
-print(tabuleiro[2])
+exibeTabuleiro()
+
+
 proxima = True
 while proxima:
-    jogadores = ['X', 'O']
-    for jogador in jogadores:
-        jogada = input(f'Digite a posição que deseja jogar {jogador}: ')
-        tabuleiro[int(jogada[0])][int(jogada[1])] = jogador
-        n = 0
-        for linha in tabuleiro:
-            print(tabuleiro[n])
-            n += 1
+    linha_coluna = input(f'Digite a posição que deseja jogar {jogador}: ')
+    try:
+        jogador = jogada(int(linha_coluna[0]), int(linha_coluna[1]))
+    except ValueError:
+        print('Os valores devem ser numeros inteiros')
+    except IndexError:
+        print('Digite uma posição válida')
+    n = 0
+    exibeTabuleiro()
 
-        for linha in tabuleiro:
-            if all(elm == 'X' for elm in linha):
-                print('O jogador X venceu!')
-                proxima = False
-            elif all(elm == 'O' for elm in linha):
-                print('O jogador O venceu!')
-                proxima = False
-        for elm in range(3):
-            if tabuleiro[0][elm] == 'X' and tabuleiro[1][elm] == 'X' and tabuleiro[2][elm] == 'X':
-                print('O jogador X venceu!')
-                proxima = False
-            if tabuleiro[0][elm] == 'O' and tabuleiro[1][elm] == 'O' and tabuleiro[2][elm] == 'O':
-                print('O jogador O venceu!')
-                proxima = False
-    
+    for linha in tabuleiro:
+        if all(elm == 'X' for elm in linha):
+            print('O jogador X venceu!')
+            proxima = False
+        elif all(elm == 'O' for elm in linha):
+            print('O jogador O venceu!')
+            proxima = False
+    for elm in range(3):
+        if (
+        tabuleiro[0][elm] == 'X' and 
+        tabuleiro[1][elm] == 'X' and 
+        tabuleiro[2][elm] == 'X'):
+            print('O jogador X venceu!')
+            proxima = False
+        if (
+        tabuleiro[0][elm] == 'O' and 
+        tabuleiro[1][elm] == 'O' and 
+        tabuleiro[2][elm] == 'O'):
+            print('O jogador O venceu!')
+            proxima = False
+    if (
+        tabuleiro[0][0] == 'X' and 
+        tabuleiro[1][1] == 'X' and 
+        tabuleiro[2][2] == 'X'
+        ) or (
+            tabuleiro[0][2] == 'X' and 
+            tabuleiro[1][1] == 'X' and 
+            tabuleiro[2][0] == 'X'
+            ):
+        print('O jogador X venceu')
+        proxima = False
+    if (
+        tabuleiro[0][2] == 'O' and 
+        tabuleiro[1][1] == 'O' and 
+        tabuleiro[2][0] == 'O'
+        ) or (
+            tabuleiro[0][0] == 'O' and 
+            tabuleiro[1][1] == 'O' and 
+            tabuleiro[2][2] == 'O'
+            ):
+        print('O jogador O venceu')
+        proxima = False
+    cont_empate = 0
+    for linha in tabuleiro:
+        if all(elm in ('X', 'O') for elm in linha):
+            cont_empate += 1
+    if cont_empate == 3:
+        print('Jogo finalizado')
+        proxima = False
